a0c2e128da37ba17138e59eae244c0465f23a488
Vitorv7 <viitor.dasilvaa@gmail.com>
2023-11-13 14:48:23 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# PROJETO: LOKI PART 2
+
+![](./assets/img/Loki.png)
+
+## TECNOLIGIAS ULTILIZADAS
+
+- HTML
+- CSS3
+- JAVASCRIPT
+
+[**BLIBIOTECA DO SCROLLREAVEAL ULTILIZANDO JS** ](https://scrollrevealjs.org/guide/installation.html)
+
+_O site está funcionando para dispositivos menores🫡_
---
+++ index.js
@@ -0,0 +1,15 @@
+window.sr = ScrollReveal({ reset: true })
+
+ScrollReveal().reveal('.title-subtilte', {
+  duration: 2000
+})
+
+ScrollReveal().reveal('.synopse', {
+  duration: 2000,
+  rotate: { x: 0, y: 0, z: 0 }
+})
+
+ScrollReveal().reveal('.mobile', {
+  duration: 2000,
+  rotate: { x: 0, y: 80, z: 0 }
+})
